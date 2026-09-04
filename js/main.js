a893aee0e3dbfdf6484c31017c45fe198aa21ea2
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-09-04 21:33:58 +0300

    Fix comment formatting in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* 
   ZDAGA FITNESS CLUB - Modern Fight Gym Engine
-*/.........sdfa.dfa.as.f
+*/.........sdfa.dfa.as.
 document.addEventListener('DOMContentLoaded', () => {
   initMobileNav();
   initFightBell();
