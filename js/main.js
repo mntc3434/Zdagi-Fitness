73e097ab0146826c7420d263328112e43ab920ce
Mintesinot getu <148215622+mntc3434@users.noreply.github.com>
2026-09-04 21:34:48 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* 
   ZDAGA FITNESS CLUB - Modern Fight Gym Engine
-*/.........sdfa.df
+*/.........sdfa.d
 document.addEventListener('DOMContentLoaded', () => {
   initMobileNav();
   initFightBell();
